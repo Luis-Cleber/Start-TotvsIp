51c7be863ce777d45fd91ff8b9f0013fffd12882
Luis Cleber <Luis.Cleber@outlook.pt>
2022-01-25 22:00:36 -0300

    projeto start-totvs

---
+++ menu.js
@@ -0,0 +1,28 @@
+
+let show = true; 
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
+        /*============================================*/ 
+           /* AQUI ESTÁ A ANIMAÇÃO DOS CONHECIMENTOS*/
+        /*============================================*/
+
+     $(function(){
+              $('.chart').easyPieChart({
+                size: 160,
+                barColor: "#36e617",
+                scaleLength: 0,
+                lineWidth: 15,
+                trackColor: "#525151",
+                lineCap: "circle",
+                animate: 2000,
+              });
+            });
