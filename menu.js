4021616f4bdf40494d1dbc2f09832b3565817411
Luis Cleber <Luis.Cleber@outlook.pt>
2022-01-27 22:04:11 -0300

    primeiro commit

--- menu.js
+++ menu.js
@@ -26,3 +26,71 @@ menuToggle.addEventListener("click", () => {
                 animate: 2000,
               });
             });
+
+
+
+            (function($) {
+
+              RemoveTableRow = function(handler) {
+                var tr = $(handler).closest('tr');
+               
+            
+                tr.fadeOut(400, function(){ 
+                  tr.remove(); 
+               
+                }); 
+                            
+                return false;
+              };
+              
+              AddTableRow = function() {
+                  
+                  var newRow = $("<tr>");
+                  var cols = "";
+                  
+                  cols += '<td>';
+                  cols += '<button class="btn btn-large btn-danger" onclick="RemoveTableRow(this)" type="button"><i class="fa fa-trash fa-lg"></i></button>';
+                  cols += '</td>';
+
+                  cols += '<td><select name="categoria">'; 
+                  cols += '<option value="pedagio" name="pedagio">Pedagio</option>';
+                  cols += '<option value="hospedagem" name="hospeagem">Hospedagem</option>';
+                  cols += ' <option value="combustivel" name="combustivel">Combustível</option>';
+                  cols += ' <option value="alimentacao" name="alimentacao">Alimentação</option>';
+                  cols += ' <option value="outra" name="outra">Outra</option>';
+                  cols += '</select></td>';
+
+                  cols += '<td><input type="text" name="descricao"></td>';
+            
+                  cols += '<td><input type="number" name="vlr.unitario"></td>'; 
+            
+                  cols += '<td><input type="number" name="quantidade"></td>'; 
+            
+                  cols += '<td><input type="number" name="vlr.total"></td>'; 
+                  
+                  
+                  
+                  newRow.append(cols);
+                  
+                  $("#products-table").append(newRow);
+                
+                  return false;
+              };
+              
+            
+            })(jQuery);
+
+            document.querySelector('.alert-confirm').onclick = function(){
+              swal({
+                          title: "Tem a certeza?",
+                          text: "Se eliminar não voltará a ver este conteudo!",
+                          type: "warning",
+                          showCancelButton: true,
+                          confirmButtonClass: "btn-danger",
+                          confirmButtonText: "Sim, apagar isto!",
+                          closeOnConfirm: false
+                      },
+                      function(){
+                          swal("Apagado!", "A Informação foi apagada permanentemente.", "success");
+                      });
+          };
